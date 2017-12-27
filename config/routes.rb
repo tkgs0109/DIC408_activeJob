e970c322f7cea26e4cbc675c07949cde0d728e55
Takemichi Tsukagoshi <subtkgs@gmail.com>
2017-12-27 12:56:34 +0000

    letter opener web

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
