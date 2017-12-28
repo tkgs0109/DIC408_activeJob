8eef731bb754756c4787036541ecf9f57ab74575
tkgs0109 <subtkgs@gmail.com>
2017-12-28 15:47:36 +0900

    Test code for NotceMailer

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,6 +20,7 @@ class BlogsController < ApplicationController
     respond_to do |format|
       if @blog.save
         # TitlesChangeJob.perform_later(@blog.id)
+        TitlesChangeJob.set(wait_until: Date.tomorrow.noon).perform_later(@blog.id)
         NoticeMailer.sendmail_blog(@blog).deliver_later
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
         format.json { render :show, status: :created, location: @blog }
